baf68a5403090c4d0830e519fa06df55e107f905
Henrique Torres Andrade <henriquetorresm@gmail.com>
2024-08-23 14:13:34 -0300

    Finalizando a regra de negócio da tela de negociação, tela de resumo do contrato e também a lógica de geração do arquivo do contrato. Feito ajustes de responsividade dos templates atuais.

--- contract_generator/app_cg/views.py
+++ contract_generator/app_cg/views.py
@@ -1,3 +1,4 @@
+import ast
 import os
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
@@ -5,6 +6,7 @@ from django.views import View
 from django.http import HttpResponse
 from django.template.loader import render_to_string
 from xhtml2pdf import pisa
+from datetime import date as dt
 
 #from contract_generator.contract_generator import settings
 from django.conf import settings
@@ -57,19 +59,22 @@ def trading_data(request):
     entryTime = request.POST.get('entry-time') # Resultado de exemplo: 18:51
     departureTime = request.POST.get('departure-time') # Resultado de exemplo: 18:51
     eventType = request.POST.get('event-type')
+    numberOfPeople = request.POST.get('number-of-people')
     eventValue = request.POST.get('event-value')
     antecipatedValue = request.POST.get('antecipated-value')
 
     if request.POST:
         for key in request.POST:
-            # Verifica se a chave começa com 'other-items'
+             #Verifica se a chave começa com 'other-items'
             if key.startswith('other-items'):
                 otherItems[key] = request.POST.get(key)
 
+    otherItemsList = []
+    for value in otherItems.values():
+        if value is not None and value != '':
+            otherItemsList.append(f'{value}')
 
-
-
-    return redirect(f'/resumo-do-contrato/?name={name}&address={address}&cpf={cpf}&phone={phone}&have10tables={have10tables}&checkSeparateTables={checkSeparateTables}&squareTables={squareTables}&roundTables={roundTables}&checkSeparateChairs={checkSeparateChairs}&amountChairs={amountChairs}&checkSeparateTowels={checkSeparateTowels}&amountTowels={amountTowels}&otherItems={otherItems}&date={date}&entryTime={entryTime}&departureTime={departureTime}&eventType={eventType}&eventValue={eventValue}&antecipatedValue={antecipatedValue}')
+    return redirect(f'/resumo-do-contrato/?name={name}&address={address}&cpf={cpf}&phone={phone}&have10tables={have10tables}&checkSeparateTables={checkSeparateTables}&squareTables={squareTables}&roundTables={roundTables}&checkSeparateChairs={checkSeparateChairs}&amountChairs={amountChairs}&checkSeparateTowels={checkSeparateTowels}&amountTowels={amountTowels}&otherItems={otherItems}&otherItemsList={otherItemsList}&date={date}&entryTime={entryTime}&departureTime={departureTime}&eventType={eventType}&numberOfPeople={numberOfPeople}&eventValue={eventValue}&antecipatedValue={antecipatedValue}')
 
 def summary_contract(request):
     name = request.GET.get('name', '')
@@ -85,10 +90,13 @@ def summary_contract(request):
     checkSeparateTowels = request.GET.get('checkSeparateTowels')
     amountTowels = request.GET.get('amountTowels')
     otherItems = request.GET.get('otherItems')
+    otherItemsList = request.GET.get('otherItemsList')
+    otherItemsList = ast.literal_eval(otherItemsList)
     date = request.GET.get('date')
     entryTime = request.GET.get('entryTime')
     departureTime = request.GET.get('departureTime')
     eventType = request.GET.get('eventType')
+    numberOfPeople = request.GET.get('numberOfPeople')
     eventValue = request.GET.get('eventValue')
     antecipatedValue = request.GET.get('antecipatedValue')
 
@@ -105,10 +113,12 @@ def summary_contract(request):
                'checkSeparateTowels':checkSeparateTowels,
                'amountTowels':amountTowels,
                'otherItems':otherItems,
+               'otherItemsList':otherItemsList,
                'date':date,
                'entryTime':entryTime,
                'departureTime':departureTime,
                'eventType':eventType,
+               'numberOfPeople':numberOfPeople,
                'eventValue':eventValue,
                'antecipatedValue':antecipatedValue,
                }
@@ -116,7 +126,7 @@ def summary_contract(request):
     return render(request,'cg/new_contract/resumo.html',context)
 
 def generate_pdf(request):
-    template_path = 'cg/new_contract/teste.html' #template_contrato.html
+    template_path = 'cg/new_contract/template_contrato.html' #template_contrato.html
 
     name = request.GET.get('name')
     address = request.GET.get('address')
@@ -130,34 +140,49 @@ def generate_pdf(request):
     amountChairs = request.GET.get('amountChairs')
     checkSeparateTowels = request.GET.get('checkSeparateTowels')
     amountTowels = request.GET.get('amountTowels')
-    otherItems = request.GET.get('otherItems')
+    #otherItems = request.GET.get('otherItems')
+    otherItemsList = request.GET.get('otherItemsList')
+    otherItemsList = ast.literal_eval(otherItemsList)
     date = request.GET.get('date')
+    print(f'===============================\n  Lista: {date}\n  Tipo: {type(date)} \n=============================== ')
+    day, month, year = transforma_data(date)
     entryTime = request.GET.get('entryTime')
     departureTime = request.GET.get('departureTime')
     eventType = request.GET.get('eventType')
+    numberOfPeople = request.GET.get('numberOfPeople')
     eventValue = request.GET.get('eventValue')
     antecipatedValue = request.GET.get('antecipatedValue')
+    currentDate = dt.today()
+    currentDate = str(currentDate)
+    currentDay, currentMonth, currentYear = transforma_data(currentDate)
+    fileName = ''.join(['_' if i == ' ' else i for i in name])
 
-    if name is None: name = '__________________________'
-    if address is None: address = '____________________________________'
-    if cpf is None: cpf = '__________________'
+
+    if name is None or name == '': name = '__________________________'
+    if address is None or address == '': address = '____________________________________'
+    if cpf is None or cpf == '': cpf = '__________________'
     itemhave10tables = '10 jogos de mesas quadradas (fornecido pelo espaço);' if have10tables == 'True' else ''
     havesquaretables = f'{squareTables} mesas quadradas avulsas;' if checkSeparateTables == 'True' and squareTables is not '' else ''
     haveroundtables = f'{roundTables} mesas redondas avulsas;' if checkSeparateTables == 'True' and roundTables is not '' else ''
     haveamountchairs = f'{amountChairs} cadeiras avulsas;' if checkSeparateChairs == 'True' and amountChairs is not '' else ''
     haveamounttowels = f'{amountTowels} cadeiras avulsas;' if checkSeparateTowels == 'True' and amountTowels is not '' else ''
-    haveotheritems = f'{otherItems}' if otherItems is not '' else ''
-    #otherItems = '<li>'+otherItems
-    # usar 
-    if phone is None: phone = '_____________________'
-    if date is None: date = '__________________________'
-    if entryTime is None: entryTime = '__________________________________'
-    if departureTime is None: departureTime = '__________________________________'
-    if eventType is None: eventType = '__________________________'
-    if eventValue is None: eventValue = '_____________'
-    if antecipatedValue is None: antecipatedValue = '_____________'
-
-    #print(f'================\n  checkSeparateTables = {checkSeparateTables} \n  squareTables = {squareTables}\n  havesquaretables = {havesquaretables} \n================')
+    if otherItemsList is None: otherItemsList = ''
+    if phone is None or phone == '': phone = '_____________________'
+    if date is None or date == '': date = '__________________________'
+    if day is None or day == '': day = '_____'
+    if month is None or month == '': month = '__________________'
+    if year is None or year == '': year = '_________'
+    if entryTime is None or entryTime== '': entryTime = '__________________________________'
+    if departureTime is None or departureTime == '': departureTime = '__________________________________'
+    if eventType is None or eventType == '': eventType = '__________________________'
+    if numberOfPeople is None or numberOfPeople == '': numberOfPeople = '__________'
+    if eventValue is None or eventValue== '': eventValue = '_____________'
+    if antecipatedValue is None or antecipatedValue == '': antecipatedValue = '_____________'
+    if currentDay is None or currentDay == '': currentDay = '_____'
+    if currentMonth is None or currentMonth == '': currentMonth = '__________________'
+    if currentYear is None or currentYear == '': currentYear = '_________'
+    
+    print(f'===============================\n Data: {currentDate} \n Type: {type(currentDate)}\n=============================== ')
 
     context = {'name':name,
                'address':address,
@@ -172,14 +197,19 @@ def generate_pdf(request):
                'roundTables':roundTables,
                'amountChairs':amountChairs,
                'amountTowels':amountTowels,
-               'haveotheritems':haveotheritems,
-               'otherItems':otherItems,
-               'date':date,
+               'otherItemsList':otherItemsList,
+               'day':day,
+               'month':month,
+               'year':year,
                'entryTime':entryTime,
                'departureTime':departureTime,
                'eventType':eventType,
+               'numberOfPeople':numberOfPeople,
                'eventValue':eventValue,
                'antecipatedValue':antecipatedValue,
+               'currentDay':currentDay, 
+               'currentMonth':currentMonth, 
+               'currentYear':currentYear
                }
     
     # Renderizar o template em HTML
@@ -187,7 +217,7 @@ def generate_pdf(request):
     
     # Criar um response como PDF
     response = HttpResponse(content_type='application/pdf')
-    response['Content-Disposition'] = 'inline; filename="arquivo.pdf"'
+    response['Content-Disposition'] = f'inline; filename="Contrato-Star-Dokmus-{fileName}.pdf"'
     
     # Criar o PDF
     pisa_status = pisa.CreatePDF(html, dest=response)
@@ -196,3 +226,32 @@ def generate_pdf(request):
         return HttpResponse('Erro ao gerar PDF', status=400)
     
     return response
+
+
+def transforma_data(date):
+    c = 0
+    day = ''
+    month = ''
+    year = ''
+    for i in date:
+        if i == '-': c+=1
+        if c == 0 and i != '-':year+=i 
+        if c == 1 and i != '-':month+=i
+        if c == 2 and i != '-':day+=i
+    if month == '01': month = 'Janeiro'
+    elif month == '02': month = 'Fevereiro'
+    elif month == '03': month = 'Março'
+    elif month == '04': month = 'Abril'
+    elif month == '05': month = 'Maio'
+    elif month == '06': month = 'Junho'
+    elif month == '07': month = 'Julho'
+    elif month == '08': month = 'Agosto'
+    elif month == '09': month = 'Setembro'
+    elif month == '10': month = 'Outubro'
+    elif month == '11': month = 'Novembro'
+    elif month == '12': month = 'Dezembro'
+
+    print(f'===============================\n Day: {day}\n Month: {month}\n Year: {year}\n=============================== ')
+
+
+    return day, month, year
