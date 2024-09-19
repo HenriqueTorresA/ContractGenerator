573a8f32bb444043bebfe9e51c47afa9a4cce8af
Henrique Torres Andrade <henriquetorresm@gmail.com>
2024-09-18 23:26:40 -0300

    Tentando adicionar Postgrees na Vercel

--- contract_generator/settings.py
+++ contract_generator/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 import os
-from pathlib import Path
+from pathlib import Path# settings.py
+from dotenv import load_dotenv
+load_dotenv()
 
 
 
@@ -79,10 +81,20 @@ WSGI_APPLICATION = 'contract_generator.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get("DB_NAME"),
+        'USER': os.environ.get("DB_USER"),
+        'PASSWORD': os.environ.get("DB_PASSWORD"),
+        'HOST': os.environ.get("DB_HOST"),
+        'PORT': os.environ.get("DB_PORT"),
     }
 }
 
