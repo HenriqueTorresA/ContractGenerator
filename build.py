ea1d1c0f8325dd8ed9fee5662048c5b8d240c8c8
Henrique Torres Andrade <henriquetorresm@gmail.com>
2024-10-03 00:02:16 -0300

    migrando banco de dados 2

--- build.py
+++ build.py
@@ -1,5 +1,5 @@
 import os
 
-os.system("python3 manage.py makemigrate")
+os.system("python3 manage.py makemigrations")
 os.system("python3 manage.py migrate")
 os.system("python3 manage.py collectstatic --noinput")
