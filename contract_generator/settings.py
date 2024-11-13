9f496c55309095150d465bbbfcfd80fde048e5c8
Henrique Torres Andrade <henriquetorresm@gmail.com>
2024-11-13 00:02:01 -0300

    >> correção das imagens, fazendo com que as imagens sejam coletadas direto do repositório do Git, ao invés da pasta estática do projeto

--- contract_generator/settings.py
+++ contract_generator/settings.py
@@ -159,27 +159,27 @@ PWA_APP_START_URL = '/'
 PWA_APP_STATUS_BAR_COLOR = 'default'
 PWA_APP_ICONS = [
     {
-        'src': '/static/images/contract-image-160x103.jpg',
+        'src': '/static/images/stardm.png',
         'sizes': '160x103'
     },
     {
-        'src': '/static/images/icon-192x192.png',
+        'src': '/static/images/stardm.png',
         'sizes': '192x192'
     },
     {
-        'src': '/static/images/icon-512x512.png',
+        'src': '/static/images/stardm.png',
         'sizes': '512x512'
     }
 ]
 PWA_APP_ICONS_APPLE = [
     {
-        'src': '/static/images/contract-image-160x103.jpg',
+        'src': '/static/images/stardm.png',
         'sizes': '160x103'
     }
 ]
 PWA_APP_SPLASH_SCREEN = [
     {
-        'src': '/static/images/contract-image-160x103.jpg',
+        'src': '/static/images/stardm.png',
         'media': '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)'
     }
 ]
